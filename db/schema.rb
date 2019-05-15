5b8d80304226e21f4a4a8485a6d7a6eb2303789e
Grace Choi <oiograceoio@gmail.com>
2019-05-15 19:36:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190515192751) do
+ActiveRecord::Schema.define(version: 20190515193620) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.integer  "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.string   "image"
   end
 
 end
